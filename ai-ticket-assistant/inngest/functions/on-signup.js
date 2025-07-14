aae4cfee4a3a0e418b9d456f5f6c5fab848b4154
Akshay Singh <114770287+Akshay-Singh11@users.noreply.github.com>
2025-07-14 15:52:48 +0530

    create AI  Agent Pipeline

--- ai-ticket-assistant/inngest/functions/on-signup.js
+++ ai-ticket-assistant/inngest/functions/on-signup.js
@@ -1,33 +1,33 @@
 import { inngest } from "../client";
 import User from "../models/user";
 import { NonRetriableError } from "inngest";
+import { sendEmail } from "../utils/email";
 
 export const onUserSignup = inngest.createFunction(
-    { id: "on-user-signup", retries: 5 },
-    { event: "user/signup" },
-    async ({ event, step }) => {
-        try {
-            const { email } = event.data;
-            const user = await step.run("get-user-email", async () => {
-                const userObject = await User.findOne({ email });
-                if (!userObject) {
-                    throw new NonRetriableError("User no longer exists in our database");
-                }
-                return userObject;
-            })
-            await step.run("send-welcome-email", async () => {
-                const subject = `Welcome to the app`
-                const message = `Hi`
-                    `\n\n
+  { id: "on-user-signup", retries: 5 },
+  { event: "user/signup" },
+  async ({ event, step }) => {
+    try {
+      const { email } = event.data;
+      const user = await step.run("get-user-email", async () => {
+        const userObject = await User.findOne({ email });
+        if (!userObject) {
+          throw new NonRetriableError("User no longer exists in our database");
+        }
+        return userObject;
+      });
+      await step.run("send-welcome-email", async () => {
+        const subject = `Welcome to the app`;
+        const message = `Hi,
+                \n\n
                 Thanks for signing up.We are glad to have you onboard!'
-                `
-                await sendEmail(user.email, subject, message);
-            })
-            return {success:true}
-        }
-        catch (error) {
-            console.log("Error running step",error.message)
-            return {success:false}
-        }
+                `;
+        await sendEmail(user.email, subject, message);
+      });
+      return { success: true };
+    } catch (error) {
+      console.log("Error running step", error.message);
+      return { success: false };
     }
-)
+  }
+);

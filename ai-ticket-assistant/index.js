bb125e23c4eb1e6880d9dc1ca6113f06f0eb9eb2
Akshay Singh <114770287+Akshay-Singh11@users.noreply.github.com>
2025-07-15 00:10:36 +0530

    changes and complete backend part

--- ai-ticket-assistant/index.js
+++ ai-ticket-assistant/index.js
@@ -4,9 +4,12 @@ import cors from "cors"
 import { serve } from "inngest/express"
 import userRoutes from "./routes/user.js "
 import ticketRoutes from "./routes/ticket.js"
-import {inngest} from "./inngest/client.js"
-import {onUserSignup} from "./inngest/functions/on-signup.js"
-import {onTicketCreated} from "./inngest/functions/on-ticket-create.js"
+import { inngest } from "./inngest/client.js"
+import { onUserSignup } from "./inngest/functions/on-signup.js"
+import { onTicketCreated } from "./inngest/functions/on-ticket-create.js"
+
+import dotenv from "dotenv";
+dotenv.config();
 
 const PORT = process.env.PORT || 3000
 const app = express()

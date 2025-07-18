2b284bee566260314c7e120d3416764320ee318b
Akshay Singh <114770287+Akshay-Singh11@users.noreply.github.com>
2025-07-18 10:16:30 +0530

    Change get routes in Ticket routes and update scripts node to nodemon

--- ai-ticket-assistant/routes/ticket.js
+++ ai-ticket-assistant/routes/ticket.js
@@ -4,8 +4,9 @@ import { createTicket, getTicket, getTickets } from "../controllers/ticket.js";
 
 const router = express.Router();
 
-router.get("/", authenticate, getTickets);
-router.get("/:id", authenticate, getTicket);
 router.post("/", authenticate, createTicket);
+router.get("/getTickets", authenticate, getTickets);
+router.get("/:id", authenticate, getTicket);
+
 
 export default router;

8e748edc049558e0790bfa6136887c4d11b09250
Akshay Singh <114770287+Akshay-Singh11@users.noreply.github.com>
2025-07-20 16:22:12 +0530

    complete set-up routers and checked auth

--- ai-ticket-frontend/src/main.jsx
+++ ai-ticket-frontend/src/main.jsx
@@ -1,10 +1,59 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Checkauth from "./compontents/check-auth.jsx";
+import Tickets from "./pages/tickets.jsx";
+import TicketDetailsPage from "./pages/ticket.jsx";
+import Login from "./pages/login.jsx";
+import Signup from "./pages/signup.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <Checkauth protected={true}>
+              <Tickets />
+            </Checkauth>
+          }
+        />
+         <Route
+          path="/tickets/:id"
+          element={
+            <Checkauth protected={true}>
+              <TicketDetailsPage/>
+            </Checkauth>
+          }
+        />
+        <Route
+          path="/login"
+          element={
+            <Checkauth protected={false}>
+              <Login/>
+            </Checkauth>
+          }
+        />
+        <Route
+          path="/signup"
+          element={
+            <Checkauth protected={false}>
+              <Signup/>
+            </Checkauth>
+          }
+        />
+        <Route
+          path="/admin"
+          element={
+            <Checkauth protected={true}>
+              <Admin/>
+            </Checkauth>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
